6c135ac8b861df3f32a27526ee87dc54d9801ee8
RimshaSaleem <rimshatif8@gmail.com>
2021-08-12 11:24:24 -0700

    done importing

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { ShoppingCartContext } from '../context/ShoppingCartContext';
+import { useContext } from 'react';
 
 const Navigation = props => {
 	return (
